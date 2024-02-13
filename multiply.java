cbab02c21c1e4ee648a74447a2182bd64f655cfa
dekothar <deepkothari99.dk@gmail.com>
2024-02-13 18:40:56 +0530

    f3 changes

--- multiply.java
+++ multiply.java
@@ -1,14 +1,7 @@
 public class multiply {
-<<<<<<< HEAD
-    
-    private int d;
-    int multiply(int a,int b){
-        return this.d+a*b;
-=======
     private int sd;
     int multiply(int a,int b){
-        return this.sd-a*b;
->>>>>>> feature1
+        return this.sd-a*b+2;
     
 }
 }
